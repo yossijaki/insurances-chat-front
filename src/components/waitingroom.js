d364f637baddee8afe24769e332c65ee4f9d888a
Yossijaki <yossijaki@gmail.com>
2024-06-07 17:58:28 -0600

    creating form

--- src/components/waitingroom.js
+++ src/components/waitingroom.js
@@ -1,7 +1,26 @@
+import { Col, Form, Row } from "react-bootstrap";
+
 const WaitingRoom = ({ joinChatRom }) => {
 
     const [username, setUsername] = useState();
-    const [chatRoom, setChatRoom] = useState();
+    const [chatroom, setChatroom] = useState();
+
+    return <Form onSubmit={ e=> {
+        e.preventDefault();
+        joinChatRoom(username, chatroom);
+    }}>
+        <Row className="px-5 py-5">
+            <Col sm={12}>
+                <Form.Group>
+                    <Form.Control placeholder="Username"
+                        onChange={e => setUsername(e.target.value)}/>
+
+                    <Form.Control placeholder="ChatRoom"
+                        onChange={e => setChatroom(e.target.value)}/>
+                </Form.Group>
+            </Col>
+        </Row>
+    </Form>
 }
 
 export default WaitingRoom;
